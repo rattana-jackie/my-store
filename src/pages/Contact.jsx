a0221545e753416fa7dcffcfd62dc58c02fea604
Rathana Jackie <rattana.work99@gmail.com>
2026-04-26 12:32:09 +0700

    fixed contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,23 @@
+import { useRef, useState } from "react";
 import { useTitle } from "../hook/useTitle";
 
 const Contact = () => {
   useTitle("My Store | Contact us");
+  const [first, setFirst] = useState("");
+  const [last, setLast] = useState("");
+  const [mail, setMail] = useState("");
+  const [desc, setDesc] = useState("");
+  const firstInput = useRef(null);
+  const handlSubmit = () => {
+    setFirst("");
+    setLast("");
+    setMail("");
+    setDesc("");
+    firstInput.current.focus();
+  };
   return (
     <div className="w-[80vw] m-auto grid grid-rows-2 xl:grid-cols-2 mt-25 gap-7 xl:gap-7">
-      <div className="">
+      <div>
         <img
           src="https://plus.unsplash.com/premium_photo-1675842663249-a8b70103dbaa?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="contact photo"
@@ -25,6 +38,11 @@ const Contact = () => {
                   name="first_name"
                   placeholder="first name"
                   className="border p-2 outline-0 rounded-sm w-full"
+                  autoFocus
+                  required
+                  ref={firstInput}
+                  value={first}
+                  onChange={(e) => setFirst(e.target.value)}
                 />
                 <br />
                 <label htmlFor="first_name" className="text-slate-400">
@@ -37,6 +55,9 @@ const Contact = () => {
                   name="last_name"
                   placeholder="last name"
                   className="border p-2 outline-0 rounded-sm w-full"
+                  required
+                  value={last}
+                  onChange={(e) => setLast(e.target.value)}
                 />
                 <br />
                 <label htmlFor="last_name" className="text-slate-400">
@@ -55,6 +76,9 @@ const Contact = () => {
               name="mail"
               placeholder="email"
               className="border p-2 outline-0 rounded-sm w-full"
+              required
+              value={mail}
+              onChange={(e) => setMail(e.target.value)}
             />
           </div>
           <div>
@@ -64,10 +88,17 @@ const Contact = () => {
             <textarea
               name="describe"
               className="border p-2 outline-0 rounded-sm w-full"
+              required
+              value={desc}
+              onChange={(e) => setDesc(e.target.value)}
             ></textarea>
           </div>
         </form>
-        <button className=" w-32 cursor-pointer bg-orange-600 text-white py-3 rounded-md uppercase ">
+        <button
+          type="submit"
+          className=" w-32 cursor-pointer bg-orange-600 text-white py-3 rounded-md uppercase "
+          onClick={handlSubmit}
+        >
           Submit
         </button>
       </div>
